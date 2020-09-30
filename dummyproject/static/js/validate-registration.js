bf1e762a0081b99a0b4ca9ea1b932b33c8d79c43
Cary <legaspicary@gmail.com>
2020-09-30 21:29:25 +0800

    Separate address api to an external js file, add comments to validate-registration

--- dummyproject/static/js/validate-registration.js
+++ dummyproject/static/js/validate-registration.js
@@ -2,22 +2,26 @@
     
     const forms = document.querySelector(".registration-form")
     const textFields = forms.querySelectorAll("form input[required]")
-
+    
+    //Calls necessary validation functions
     forms.addEventListener("submit", e => {
+        //Calls necessary functions if validation fails
         if(forms.checkValidity() === false){
             e.preventDefault();
             e.stopPropagation();
         }
-
+        //Bootstrap was-validated class for sucessful validation
         forms.classList.add("was-validated");
     })
 
+    //Adds event listener to all required textfields
     textFields.forEach(field => {
         field.addEventListener("input", e => {
             displayValidity(e.target)
         })
     })
 
+    //Displays the validation message/response
     const displayValidity = (field) => {
         field.classList.remove('is-valid');
         field.classList.remove('is-invalid');
@@ -27,5 +31,4 @@
         else
             field.classList.add('is-invalid');
     }
-
 })()
