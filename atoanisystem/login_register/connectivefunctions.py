7dd8c7b8c28328131d40777c1c3f539c3035acb8
Rosalijos Joshua <rosalijos.joshua1999@gmail.com>
2020-10-07 13:06:02 +0700

    improve ui status

--- atoanisystem/login_register/connectivefunctions.py
+++ atoanisystem/login_register/connectivefunctions.py
@@ -1,4 +1,26 @@
 from login_register.models import *
+import pandas as pd
+from datetime import datetime as dt
 
-def get_order_pairing(user):
-    pass
+def convert(time):
+    try:
+        return dt(time.year,time.month,time.day)
+    except:
+        return None
+
+
+def get_order_pairing(id):
+    df = pd.DataFrame(Order_Pairing.objects.filter(farmer_id=id).values())
+    df['accepted_date'] = df['accepted_date'].apply(convert)
+    return df
+
+def search_pairing(predate,postdate,df):
+    return df[(df['accepted_date'] >= predate) & (df['accepted_date'] <= postdate)]
+
+def count_status(df):
+    # assured that all order pairs have been accepted
+    accepted = (df['harvest_date'].isnull()).sum()
+    harvested = len((df.loc[(df['harvested_date'].notna()) & (df['collected_date'].isnull())]))
+    collected = (df['collected_date'].notna()).sum()
+
+    return [accepted, harvested, collected]
