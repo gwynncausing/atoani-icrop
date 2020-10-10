9138e8c4c28ee743aa4145ac104a2a964fd7bb0d
Cary <legaspicary@gmail.com>
2020-10-10 11:15:48 +0800

    Implement farmer functions to use live data from DB instead of dummy

--- atoanisystem/dashboard/farmerfunctions.py
+++ atoanisystem/dashboard/farmerfunctions.py
@@ -9,43 +9,49 @@ def get_location_str(location_id):
     else:
         return 'N/A'
 
+def format_nan_values(list,column_key):
+    for x in list:
+        if math.isnan(x[column_key]):
+            x[column_key]=None
+
 def format_location(orders):
     for order in orders:
         order['location_id'] = get_location_str(order['location_id'])
 
+def format_crop_name(orders):
+    for order in orders:
+        order['name']=Crop.objects.get(id=order['crop_id']).name
+
 def get_incoming_orders(user):
-    print("INCOMINGGG")
     incoming_orders = dashboard_utility.matching_algorithm(user.farmer.land_area)
-    print(incoming_orders)
-    # df = dashboard_utility.datatable_farmer(user.farmer)
-    # orders = dashboard_utility.display_farmer_table(df)
-    # format_location(orders)
-    # print(orders)
-    # return orders
+    format_crop_name(incoming_orders)
+    format_location(incoming_orders)
     return incoming_orders
 
 #TO BE IMPLEMENTED
 def get_reserved_orders(user):
-    # df = dashboard_utility.datatable_farmer(user.farmer)
-    # orders = dashboard_utility.display_farmer_table(df)
-    # format_location(orders)
-    # reserved_orders = []
-    # for order in orders:
-    #     if order['status'] == 'Ongoing':
-    #         reserved_orders.append(order)
-    # return reserved_orders
-    return []
+    df = dashboard_utility.datatable_farmer(user.farmer)
+    orders = dashboard_utility.display_farmer_table(df)
+    format_location(orders)
+    format_nan_values(orders,'land_area_needed')
+    print(orders)
+    reserved_orders = []
+    for order in orders:
+        if order['accepted_date'] and order['status'] != 'Collected':
+            reserved_orders.append(order)
+    return reserved_orders
 
 def get_finished_orders(user):
-    # df = dashboard_utility.datatable_farmer(user.farmer)
-    # orders = dashboard_utility.display_farmer_table(df)
-    # format_location(orders)
-    # finished_orders = []
-    # for order in orders:
-    #     if order['status'] == 'Collected':
-    #         finished_orders.append(order)
-    # return finished_orders
-    return []
+    df = dashboard_utility.datatable_farmer(user.farmer)
+    orders = dashboard_utility.display_farmer_table(df)
+    format_nan_values(orders,'land_area_needed')
+    format_location(orders)
+    finished_orders = []
+    for order in orders:
+        #CHANGE TO COLLECTED
+        if order['status']=='Collected':
+            finished_orders.append(order)
+    return finished_orders
 
 #https://docs.djangoproject.com/en/3.1/topics/db/transactions/
 #since django works in autocommit mode by default (see in Autocommit section), i assume that there will be no concurrent problems
