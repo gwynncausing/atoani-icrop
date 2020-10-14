3dceca309ac0d463b3d1681d0cee31a07a0aebbd
Cary <legaspicary@gmail.com>
2020-10-14 22:05:06 +0800

    Add backend implementation for create order in dashboard views.py for customer

--- atoanisystem/dashboard/views.py
+++ atoanisystem/dashboard/views.py
@@ -5,7 +5,7 @@ from django.views.generic import View
 from login_register.models import Order
 from . import farmerfunctions as ff
 from . import customerfunctions as cf
-from login_register.models import Order
+from login_register.models import *
 
 #########################################################
 #               Farmer related views                    #
@@ -83,7 +83,24 @@ class CustomerDashboardView(View):
     def post(self,request):
         if request.is_ajax():
             print("request was ajax")
-            
+            if request.POST.get('operation') == 'create-order':
+                customer = request.user
+                crop_id = request.POST.get('crop-id')
+                crop = Crop.objects.get(id=crop_id)
+                weight = request.POST.get('weight')
+                land_area_needed = request.POST.get('land-area-needed')
+                location = request.user.customer.location
+                street = request.POST.get('street')
+                brgy = request.POST.get('barangay')
+                city = request.POST.get('city')
+                province = request.POST.get('province')
+                if location.province != province or location.city != city or location.brgy != brgy or location.street != street:
+                    location = Location.objects.create(street=street,brgy=brgy,city=city,province=province)
+                    location.name = str(location.brgy) +', '+ str(location.city) + ', ' + str(location.province)
+                order = cf.create_order(customer,crop,weight,location,land_area_needed)
+                arr = cf.get_total_orders(request.user)
+                json = {'data':arr}
+                return JsonResponse(json)
         return redirect('login_register:login')
 
 class CustomerTotalOrdersView(View):
