38aa91b479b708a655142cd3f14fe82a987bbfec
Anthony Martin Navarez <martinpnavarez@gmail.com>
2020-10-21 13:57:31 +0800

    Changed the interval of scheduler

--- atoanisystem/login_register/scheduler/scheduler.py
+++ atoanisystem/login_register/scheduler/scheduler.py
@@ -7,12 +7,12 @@ from django_apscheduler.models import DjangoJobExecution
 import sys
 from login_register.connectivefunctions import check_obsolete_orders
 
-
+# test value for start fxn, for debugging purposes (when debugging, set hours and minutes to 0, seconds to 5)
 def start():
     scheduler = BackgroundScheduler()
     scheduler.add_jobstore(DjangoJobStore(), "default")
     # run this job every 24 hours
-    scheduler.add_job(check_obsolete_orders, 'interval', hours=0, seconds=3, name='clean_orders', jobstore='default', id="check_obsolete_orders", replace_existing=True)
+    scheduler.add_job(check_obsolete_orders, 'interval', hours=0, minutes=15, name='clean_orders', jobstore='default', id="check_obsolete_orders", replace_existing=True)
     register_events(scheduler)
     scheduler.start()
     print("Scheduler started...", file=sys.stdout)
